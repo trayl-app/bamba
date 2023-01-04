189907e54359e90780885fd5e1196f632148202a
ocintnaf <ocintnaf@gmail.com>
2023-01-04 08:30:07 +0100

    fix pipeline

--- src/components/button/default-props.ts
+++ src/components/button/default-props.ts
@@ -1,8 +1,8 @@
 import { VARIANT } from './constants';
 import { ButtonProps } from './types';
 
-export const defaultProps: Partial<ButtonProps> = {
-  variant: VARIANT.PRIMARY,
+export const defaultProps: Partial<ButtonProps> = Object.freeze({
+  variant: VARIANT.SECONDARY,
   isDisabled: false,
   isLoading: false,
-};
+});
